513c21b8c4cc48e1425116bd2c7976c889d2ec7c
Graham Driver <drivergraham1@gmail.com>
2024-02-20 23:21:59 -0600

    slidePotentiometer Drivers Finished
    This is the first finished versions of the slider potentiometers drivers.

--- Skip-Device/include/slidePotentiometersDriver.h
+++ Skip-Device/include/slidePotentiometersDriver.h
@@ -1,17 +1,59 @@
 /**
  * @file slidePotentiometersDriver.h
- * @author Graham Driver (drivergraham1@gmail.com)
- * @brief This script contains functions to initialize and control the slide potentiometers connected to the WEE Curl Skip Device.
- * @version 0.1
+ * @author Graham Driver (driverg@myumanitoba.ca)
+ * @brief header file for the slider drivers.
+ * @version 0.2
  * @date 2024-02-18
  * 
  * @copyright Copyright (c) 2024
  * 
  */
 #include <Arduino.h>
-#include <stdint.h>
+#include <stdlib.h>
 
-#define leftSlidePin    A0
-#define rightSlidePin   A99
+#define RAWindowSize    100
+#define leftSliderPin   A0
+#define rightSliderPin  A13
 
-uint8_t getRawADCLeftPotentiometer();
+typedef struct{
+    uint8_t index = 0;            // Index of the moving average
+    uint16_t data[RAWindowSize];  // Data from rolling average
+    uint32_t avgSum = 0;          // Sum of values in data
+    uint16_t pin = INT16_MAX;     // Pin of the slider
+}slider_t;
+
+typedef enum
+{
+  // Status Types (Optional to use)
+  SLIDE_POT_OK       = 0x00,  
+  SLIDE_POT_ERROR    = 0x01,
+  SLIDE_POT_BUSY     = 0x02,
+  SLIDE_POT_TIMEOUT  = 0x03
+} SLIDE_POT_StatusTypeDef;
+
+
+/**
+ * @brief This function can be called to poll the analog signal from the given slider.
+ * Should be passed slider_t type that will keep moving average of signal.
+ * 
+ * @param slider A slider struct pointer.
+ * @return SLIDE_POT_StatusTypeDef 
+ */
+SLIDE_POT_StatusTypeDef pollSlider(slider_t *slider);
+
+
+/**
+ * @brief Get the Average Pot Value
+ * 
+ * @param slider A slider struct pointer.
+ * @return uint16_t 
+ */
+uint16_t getAveragePotValue(slider_t *slider);
+
+/**
+ * @brief Get the Percentage Pot Value
+ * 
+ * @param slider A slider struct pointer.
+ * @return uint8_t
+ */
+uint8_t getPercentagePotValue(slider_t *slider);
--- Skip-Device/src/slidePotentiometersDriver.cpp
+++ Skip-Device/src/slidePotentiometersDriver.cpp
@@ -1,8 +1,8 @@
 /**
  * @file slidePotentiometersDriver.cpp
- * @author Graham Driver (drivergraham1@gmail.com)
- * @brief This script contains functions to initialize and control the slide potentiometers connected to the WEE Curl Skip Device.
- * @version 0.1
+ * @author Graham Driver (driverg@myumanitoba.ca)
+ * @brief This script is used in order to poll the sliders of the the WEE Curl Skip Device.
+ * @version 0.2
  * @date 2024-02-18
  * 
  * @copyright Copyright (c) 2024
@@ -11,11 +11,28 @@
 
 #include "slidePotentiometersDriver.h"
 
+SLIDE_POT_StatusTypeDef pollSlider(slider_t *slider){
+    SLIDE_POT_StatusTypeDef status = SLIDE_POT_OK;
+    uint16_t potValue;
+
+    //assert(slider.pin != NULL);
+
+    potValue = analogRead(slider->pin);
+    slider->avgSum -= slider->data[slider->index];
+    slider->avgSum += potValue;
+    slider->data[slider->index] = potValue;
+    slider->index++;
+    slider->index %= RAWindowSize;
+
+    error:
+        return status;
+}
 
 
-uint8_t getRawADCLeftPotentiometer(void){
-    uint8_t analogValue;
+uint16_t getAveragePotValue(slider_t *slider){
+    return (uint16_t)(slider->avgSum / RAWindowSize);
+}
 
-    analogValue = analogRead(leftSlidePin);
-    return analogValue;
+uint8_t getPercentagePotValue(slider_t *slider){
+    return (uint8_t)((float)100 * ( (float)slider->avgSum / (float)RAWindowSize)/ (float)4096);
 }
