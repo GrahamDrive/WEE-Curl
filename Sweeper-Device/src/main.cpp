a9eb66074033713ffa354ec13ba3bce776dbaee8
Richard Reyes <122465907+RiAllen120@users.noreply.github.com>
2024-03-25 11:37:34 -0500

    Update main.cpp, done wifi handler
    Need to implement pairing mode? Or preset MAC addresses...

--- Sweeper-Device/src/main.cpp
+++ Sweeper-Device/src/main.cpp
@@ -1,10 +1,9 @@
 #include <Arduino.h>
 #include "led_driver.h"
 #include "message_handler.h"
+#include "wifi_handler.h"
 
-#define __TESTING 1
 // put function declarations here:
-int myFunction(int, int);
 
 
 void setup() {
@@ -14,22 +13,11 @@ void setup() {
 
 
   led_init();
-
+	wifi_init();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  #if defined(__TESTING)
-  blink_color(RED,3);
-  blink_color(GREEN,3);
-  blink_color(BLUE,3);
-  blink_color(YELLOW,3);
-  blink_color(PINK,3);
-  blink_color(TEAL,3);
-  #endif
 }
 
 // put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
