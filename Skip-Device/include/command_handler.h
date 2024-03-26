1e579d7c324c6334ab22ea786d7ac6e49ec27c82
Richard Reyes <122465907+RiAllen120@users.noreply.github.com>
2024-03-26 14:42:29 -0500

    Update command_handler.h

--- Skip-Device/include/command_handler.h
+++ Skip-Device/include/command_handler.h
@@ -3,17 +3,17 @@
 typedef struct parameter_t param;
 
 //sweeper assignees
-#define ASSIGN_NONE 0b00
-#define ASSIGN_LEFT 0b01
-#define ASSIGN_RIGHT 0b10
+#define ASSIGN_NONE 0b0000
+#define ASSIGN_LEFT 0b0100
+#define ASSIGN_RIGHT 0b1000
 
 //sweeping strength
-#define LIGHT_STR 0b00
-#define MED_STR 0b01
-#define HARD_STR 0b10
-#define STOP 0b11
+#define LIGHT_STR 0b0000
+#define MED_STR 0b0001
+#define HARD_STR 0b0010
+#define STOP 0b0011
 //special offset can be used to implement more commands
-#define SPECIAL 0b11
+#define SPECIAL 0b1100
 
 uint16_t update_payload(param*);
 void update_left_val(param*,int);
