443d767e6c59ee2785a760e761ae8966d6204904
Graham Driver <drivergraham1@gmail.com>
2024-04-06 18:59:21 -0500

    Update wifi_handler.h
    Added sweeper definitions
    
    Added MACs for three sweepers
    Note: Sweeper One and Two need to be added.

--- Skip-Device/include/wifi_handler.h
+++ Skip-Device/include/wifi_handler.h
@@ -3,6 +3,11 @@
 #include "esp_now.h"
 #include "WiFi.h"
 
+#define SWEEPER_ONE     0
+#define SWEEPER_TWO     1
+#define SWEEPER_THREE   3
+
+
 typedef struct packet_t{
     uint8_t leftSweeperIntensity;
     uint8_t rightSweeperIntensity;
@@ -10,6 +15,10 @@ typedef struct packet_t{
     uint8_t sweeperOnRight;
     bool hurryHard;
 }Packet;
+
+uint8_t sweeperOneMac[] = {0x0C, 0xB8, 0x15, 0x77, 0xD4, 0x54};
+uint8_t sweeperTwoMac[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};     // Change to actual MACs
+uint8_t sweeperThreeMac[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};   // Change to actual MACs
 
 
 /**
