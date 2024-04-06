bad0d407d68c3968748f93dd42233a79e1fdfe77
Graham Driver <drivergraham1@gmail.com>
2024-04-06 18:29:58 -0500

    Update slidePotentiometersDriver.cpp
    Slide potentiometer function now able to return a 0-8 level based on slider position for sweeper.

--- Skip-Device/src/slidePotentiometersDriver.cpp
+++ Skip-Device/src/slidePotentiometersDriver.cpp
@@ -38,3 +38,38 @@ uint16_t getAveragePotValue(slider_t *slider){
 uint8_t getPercentagePotValue(slider_t *slider){
     return (uint8_t)((float)100 * ( (float)slider->avgSum / (float)RAWindowSize)/ (float)4096);
 }
+
+uint8_t getSweeperLevel(slider_t *slider){
+    uint8_t percentage = getPercentagePotValue(slider);
+    uint8_t sweepLevel = 0;
+
+    if(percentage <= 3){
+        sweepLevel = 0;
+    }
+    else if(percentage > 3 & percentage <= 12){
+        sweepLevel = 1;
+    }
+    else if(percentage > 12 & percentage <= 24){
+        sweepLevel = 2;
+    }
+    else if(percentage > 24 & percentage <= 36){
+        sweepLevel = 3;
+    }
+    else if(percentage > 36 & percentage <= 48){
+        sweepLevel = 4;
+    }
+    else if(percentage > 48 & percentage <= 60){
+        sweepLevel = 5;
+    }
+    else if(percentage > 60 & percentage <= 72){
+        sweepLevel = 6;
+    }
+    else if(percentage > 72 & percentage <= 84){
+        sweepLevel = 7;
+    }
+    else if(percentage > 84){
+        sweepLevel = 8;
+    }
+
+    return sweepLevel;
+}
